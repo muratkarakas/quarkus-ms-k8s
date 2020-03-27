b63c7c126aa78adf0420d753e9a739677d1ec063
Murat Karakas <murat.karakas@eteration.com>
2020-03-27 18:07:45 +0300

    build options added

--- post-query-service/build.sh
+++ post-query-service/build.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
 ./mvnw clean package -Pnative -Dquarkus.native.container-runtime=docker -Dquarkus.container-image.build=true
+
+# ./mvnw clean package -Dquarkus.kubernetes.deploy=true
+
+#  ./mvnw clean package -Dquarkus.container-image.build=true
+
+
+#./mvnw clean package -Pnative -Dquarkus.native.container-runtime=docker -Dquarkus.container-image.build=true   -Dquarkus.container-image.push=true   -Dquarkus.kubernetes.deploy=true
+
+
+#./mvnw clean package -Pnative -Dquarkus.native.container-runtime=docker -Dquarkus.container-image.build=true -Dquarkus.container-image.push=true 
